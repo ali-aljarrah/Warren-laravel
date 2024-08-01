9c5065a0ab98834ec861df1332c3ed6d4f10f372
zainab98alrez <zainab.98ta@gmail.com>
2024-08-01 16:34:19 +0300

    contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,69 +11,86 @@
      @include('include.loader')
     @include('include.menu')
 
-    <section class="py-5">
-        <div class="container-fluid block-padding">
-            <div class="row">
-                <div class="col-lg-7 mb-4 mb-lg-0">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <h1 class="fs-40 dark-color fw-bold mb-3">Contact</h1>
-                            <p class="fs-16 dark-color">
-                                If you are looking for dentists in Warren, MI, look no further than Warren Laser Dentistry for all of your dentistry needs.
-                            </p>
-                            <div class="d-flex">
-                                <a href="https://maps.app.goo.gl/acNmamWC8mGXBSRj9" target="_blank" rel="noopener noreferrer" class="btn-blue rounded-2 px-3 fs-14 me-3">
-                                    In Person
-                                </a>
-                                <a href="tel:+1-586-756-6351" class="btn btn-outline-dark">
-                                    By Phone
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-5">
-                        <h2 class="dark-color fs-24 fw-bold mb-3">A New Level of Respect for Your Family</h2>
-                        <p class="fs-16 dark-color">
+    <section class="page-bg-image shadow-bg-layer d-flex justify-content-start align-items-center " style="background-image: url(/assets/imgs/contact/banner.webp)">
+        <div class="block-padding">
+              <div class="container-fluid pb-5">
+                 <div class="row pb-5">
+                    <div class="col-lg-10 mx-auto text-center py-5 my-5">
+                        <h1 class="white-color-1 py-4 fs-32 fw-bold d-flex justify-content-center align-items-center">
+                            A New Level of Respect for Your Family
+                        </h1>
+                        <p class="white-color fs-16 text-center">
                             If you are looking for dentists in Warren, look no further than Warren Laser Dentistry for all of your dentistry needs. We are proud to have a highly educated & professional roster of dental experts that can help with any & all of your dental care needs & can do so right here in your own community to make it easy on you.
                         </p>
-                        <p class="fs-16 dark-color">
+                        <p class="white-color fs-16 text-center">
                             In addition to our advanced technologies & innovative techniques, we’ve revolutionized dentistry to be truly personalized & convenient. We have hours that fit your busy schedule. Plus, we take the time to listen & clearly explain everything you need to know about your dental care.
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-5 flex-grow-1">
-                    <div class="h-100 d-flex justify-content-center align-items-center">
-                        <div>
+              </div>
+          </div>
+      </section>
+
+      <section class="py-5">
+        <div class="block-padding p-sm-0">
+              <div class="container-fluid p-lg-5">
+                 <div class="row d-flex justify-content-center align-items-center m-top-minus2 size-media">
+                    <div class="col-lg-7 px-lg-5">
+                        <div class="h-100 d-flex justify-content-start align-items-center">
+                            <div class="contact-dark px-5 pt-5">
+                                <div class="ps-lg-5 mx-lg-4 mb-lg-0 mb-5">
+                                    <h1 class="fs-40 white-color fw-bold mb-3 pt-5">
+                                        Contact
+                                    </h1>
+                                    <p class="fs-16 white-color pe-5 me-lg-5 pt-3">
+                                        If you are looking for dentists in Warren, MI, look no further than Warren Laser Dentistry for all of your dentistry needs.
+                                    </p>
+                                    <div class="d-flex pt-3">
+                                        <a href="https://maps.app.goo.gl/acNmamWC8mGXBSRj9" target="_blank" rel="noopener noreferrer" class="btn btn-light rounded-2 px-3 pt-2 fs-14 me-3">
+                                            In Person
+                                        </a>
+                                        <a href="tel:+1-586-756-6351" class="btn btn-outline-light">
+                                            By Phone
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div class="col-1">
                             <div class="working-hours-block">
                                 Working Hours
                             </div>
-                            <div class="working-hours-wrapper pt-4">
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Monday: </span>9am-6pm
-                                </div>
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Tuesday: </span>9am-6pm
-                                </div>
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Wednesday: </span>Closed
-                                </div>
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Thursday: </span>9am-6pm
-                                </div>
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Friday: </span>9am-4pm
-                                </div>
-                                <div class="fs-20 dark-color mb-4">
-                                    <span class="fw-bold">Saturday: </span>8am-2pm
+                      </div>
+                      <div class="col-4 p-0">
+                        <div class="h-100 d-flex justify-content-start align-items-center">
+                            <div>
+                                <div class="working-hours-wrapper">
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Monday: </span>9am-6pm
+                                    </div>
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Tuesday: </span>9am-6pm
+                                    </div>
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Wednesday: </span>Closed
+                                    </div>
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Thursday: </span>9am-6pm
+                                    </div>
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Friday: </span>9am-4pm
+                                    </div>
+                                    <div class="fs-20 dark-color mb-4">
+                                        <span class="fw-bold">Saturday: </span>8am-2pm
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
+              </div>
+          </div>
+      </section>
 
     <section class="py-5">
       <div class="container-fluid p-0">
@@ -85,31 +102,31 @@
 
     <section class="py-5 contact-form-bg">
       <div class="container-fluid block-padding">
-        <div class="row">
+        <div class="row d-flex justify-content-center align-items-center py-5">
             <div class="col-lg-6 mb-lg-0">
-                <div class="mb-5">
-                    <h3 class="dark-color-1 mb-4 fs-24 fw-bold">
+                <div class="mb-5 pt-5">
+                    <h3 class="white-color-1 mb-4 fs-24 fw-bold">
                         Appointments to Fit Your Schedule
                     </h3>
-                    <p class="dark-color fs-16">
+                    <p class="white-color fs-16">
                         At Warren Laser Dentistry, we provide your family with the dental care you need without having to take time off from work or school for your dentist appointment. We understand that you, our valued patients, lead busy lives & it can be difficult to put aside an hour out of your busy day for a teeth cleaning. That is why we do all we can to open our schedule to allow you to come in when it doesn’t interfere with your day-to-day responsibilities—It’s just another way we show our patients how much we care.
                     </p>
                 </div>
                 <div class="mb-5">
-                    <h4 class="dark-color-1 mb-4 fs-24 fw-bold">
+                    <h4 class="white-color-1 mb-4 fs-24 fw-bold">
                         Saturday Appointments
                     </h4>
-                    <p class="dark-color fs-16">
+                    <p class="white-color fs-16">
                         It seems like our patients are busier & busier lately. With all the running around that comes with modern life, it’s easy to let your obligation to your smile fall through the cracks. That’s why we’re committed to making dental care as convenient & accessible as possible by being a dentist that's open on weekends.
                     </p>
-                    <p class="dark-color fs-16">
+                    <p class="white-color fs-16">
                         We believe that dentists that are open on Saturdays offer better access to preventive care, such as cleanings & exams. Better access to preventive care means fewer big dental problems in the future, which means less discomfort, pain & expense. We don’t want you to have to choose between work or school & keeping your smile healthy.
                     </p>
                 </div>
             </div>
             <div class="col-lg-5 offset-lg-1 mb-5 mb-lg-0">
-                <div class="contact-form-wrapper">
-                    <h5 class="fs-24 dark-color-1 fw-600 mb-4">Get in touch</h5>
+                <div class="contact-form-wrapper p-4">
+                    <h5 class="fs-28 dark-color-1 fw-600 mb-4">Get in touch</h5>
                     <form id="emailForm" action="{{route('sendEmail')}}" method="POST">
                         @csrf
                         <div class="row mb-4">
