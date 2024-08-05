d3627a16253e9dd50ae199a8fa8907fa1c75fd03
zainab98alrez <zainab.98ta@gmail.com>
2024-08-05 15:24:19 +0300

    loader

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
   <head>
     <style>
-       #loader,#loader.show{transition:.3s ease-in-out}#loader{z-index:9999;position:fixed;top:0;bottom:0;left:0;right:0;display:flex;justify-content:center;align-items:center;opacity:0;visibility:hidden;background-color:#fff}#loader.show{opacity:1;visibility:visible}.loader-body{position:relative}
-    </style>
+          #loader,#loader.show{transition:.3s ease-in-out}#loader{z-index:9999;position:fixed;top:0;bottom:0;left:0;right:0;display:flex;justify-content:center;align-items:center;opacity:0;visibility:hidden;background-color:#fff}#loader.show{opacity:1;visibility:visible}.loader-body{position:relative}.loader svg polyline{fill:none;stroke-width:3;stroke-linecap:round;stroke-linejoin:round}.loader svg polyline#back{fill:none;stroke:#c3c3c3}.loader svg polyline#front{fill:none;stroke:#05395b;stroke-dasharray:48,144;stroke-dashoffset:192;animation:1.4s linear infinite dash_682}@keyframes dash_682{72.5%{opacity:0}to{stroke-dashoffset:0}}
+   </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- resources/views/include/loader.blade.php
+++ resources/views/include/loader.blade.php
@@ -1,7 +1,15 @@
 <div id="loader" class="show">
     <div class="loader-body">
-        <div>
+        {{-- <div>
             <img class="load" loading="lazy" width="200" height="200" src="{{asset('/assets/imgs/Heart beat.gif')}}" alt="WARREN - Experience the difference of truly personal care!">
+          </div> --}}
+
+          <div class="loader">
+            <svg width="64px" height="48px">
+                <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+              <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+            </svg>
           </div>
+
     </div>
 </div>
